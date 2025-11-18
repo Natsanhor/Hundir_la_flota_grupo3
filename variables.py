375488674d05b2dafed015888bebdf3ee496389f
franrubiorubio <franrubiorubio8@gmail.com>
2025-11-18 12:33:21 +0100

    a

---
+++ variables.py
@@ -0,0 +1,17 @@
+import numpy as np
+import random 
+from clases import *
+
+lista_barcos = {
+    "lancha1": 1,
+    "lancha2": 1,
+    "lancha3": 1,
+    "lancha4": 1,
+    "patrullero1": 2,
+    "patrullero2": 2,
+    "patrullero3": 2,
+    "submarino1": 3,
+    "submarino2": 3,
+    "acorazado": 4
+}
+
